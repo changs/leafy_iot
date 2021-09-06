aa3dd797d191b7291467bf9f24cf078075d422c3
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-09-06 21:13:05 +0200

    Update bme simple program

--- leafy_iot.ino
+++ leafy_iot.ino
@@ -1,13 +1,16 @@
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(7, OUTPUT); 
+    Serial.begin(9600);
+    while(!Serial);    // time to get serial running
+    Serial.println(F("BME280 test"));
 }
 
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(7, HIGH);
-    delay(2000);
+    delay(3000);
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(7, LOW);
-    delay(2000);
+    delay(3000);
 }
